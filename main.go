c1fb92251ef4253c0fe56ed03e47ed21405948e2
xekaterina <145139183+xekaterina@users.noreply.github.com>
2023-10-08 10:26:15 +0300

    bigUpdate
    moving files to db

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
-	"log"
-	"os"
-	"strconv"
-	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type Book struct {
@@ -24,55 +23,32 @@ func check(e error) {
 
 func main() {
 
-	entries, err := os.ReadDir("./books")
+	db, err := sql.Open("sqlite3", "books/books.sqlite")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("SELECT Name, Author, Genre, Year, Rating FROM Books")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	books := []Book{}
+
+	_, err = db.Exec("insert into books (Name, Author, Genre, Year, Rating) values ('War and Peace', 'Leo Tolstoy', 'Historical novel', 1865, 7)")
+	if err != nil {
+		panic(err)
 	}
 
-	books := make([]Book, 0, len(entries))
-
-	for _, e := range entries {
-
-		name := e.Name()
-
-		dat, err := os.ReadFile("books/" + name)
-		check(err)
-
-		fileLines := strings.Split(string(dat), "\n")
-		// fmt.Println(fileLines[1])
-
-		dirtyGenre := strings.Split(fileLines[2], " ")
-		genre := (strings.Join(dirtyGenre[1:], " "))
-
+	for rows.Next() {
+		book := Book{}
+		err := rows.Scan(&book.Name, &book.Author, &book.Genre, &book.Year, &book.Rating)
 		if err != nil {
-			// ... handle error
-			panic(err)
+			fmt.Println(err)
+			continue
 		}
-
-		dirtyYear := strings.Split(fileLines[3], " ")
-		year, err := strconv.Atoi(dirtyYear[1])
-		if err != nil {
-			// ... handle error
-			panic(err)
-		}
-
-		dirtyRating := strings.Split(fileLines[4], " ")
-		rating, err := strconv.Atoi(dirtyRating[1])
-		if err != nil {
-			// ... handle error
-			panic(err)
-		}
-
-		var book = Book{Name: name, Author: fileLines[1], Genre: genre, Year: year, Rating: rating}
 		books = append(books, book)
 	}
 	fmt.Println(books)
-
-	// books := make([]Book, 0, 5)
-	// dat, err := os.ReadFile("books/test_1")
-	// check(err)
-	// fmt.Print(string(dat))
-
-	// // Output: [a b c]
 
 }
